1ea1059269f72cac167b31d8fa37cb7a28785f9b
hojiahao <hojiahao@outlook.com>
2024-12-28 16:45:37 +0800

    first commit

---
+++ src/test/java/cn/edu/szu/train/TrainTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.szu.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
