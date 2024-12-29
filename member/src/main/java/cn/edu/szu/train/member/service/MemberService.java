3e1dc0d8a7a83edf081a8139b47204de65ff4077
hojiahao <hojiahao@outlook.com>
2024-12-29 23:22:17 +0800

    完成会员注册接口的开发

--- member/src/main/java/cn/edu/szu/train/member/service/MemberService.java
+++ member/src/main/java/cn/edu/szu/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package cn.edu.szu.train.member.service;
 
+import cn.edu.szu.train.member.domain.Member;
+import cn.edu.szu.train.member.domain.MemberExample;
 import cn.edu.szu.train.member.mapper.MemberMapper;
+import cn.hutool.core.collection.CollUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +16,21 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> memberList = memberMapper.selectByExample(memberExample);
+
+        if (CollUtil.isNotEmpty(memberList)) {
+            throw new RuntimeException("Current mobile phone number is registered.");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
