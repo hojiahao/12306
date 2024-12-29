c0327c1b5deb66df6e89fdd4186d15be48859413
hojiahao <hojiahao@outlook.com>
2024-12-29 18:25:28 +0800

    集成持久层Mybatis，访问数据库成功

---
+++ member/src/main/java/cn/edu/szu/train/member/controller/MemberController.java
@@ -0,0 +1,19 @@
+package cn.edu.szu.train.member.controller;
+
+import cn.edu.szu.train.member.service.MemberService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/count")
+    public int count() {
+        return memberService.count();
+    }
+}
---
+++ member/src/main/java/cn/edu/szu/train/member/mapper/MemberMapper.java
@@ -0,0 +1,5 @@
+package cn.edu.szu.train.member.mapper;
+
+public interface MemberMapper {
+    int count();
+}
