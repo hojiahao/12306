111fc516286ae33bc14b60f0d8f0905307df18a2
hojiahao <hojiahao@outlook.com>
2025-01-05 16:17:17 +0800

    增加Axios拦截器配置，打印请求参数和返回结果。

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/reset.css'
 import * as Icons from '@ant-design/icons-vue'
+import axios from 'axios'
 
 const app = createApp(App)
 app.use(Antd).use(store).use(router).mount('#app')
@@ -14,3 +15,20 @@ const icons = Icons
 for (const icon in icons) {
     app.component(icon, icons[icon])
 }
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use((config) => {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+})
+axios.interceptors.response.use((response) => {
+    console.log('返回结果：', response.data);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+})
