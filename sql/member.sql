cda5a89e72b91688d725829c039ce1b8efb9c153
hojiahao <hojiahao@outlook.com>
2025-01-19 14:01:21 +0800

    乘车人表的设计

--- sql/member.sql
+++ sql/member.sql
@@ -5,3 +5,16 @@ CREATE TABLE `member` (
     primary key (`id`),
     unique key `mobile_unique` (`mobile`)
 ) engine=innodb default charset=utf8mb4 comment='membership';
+
+drop table if exists `passenger`;
+create table `passenger` (
+    `id` bigint not null comment 'id',
+    `member_id` bigint not null comment 'member id',
+    `name` varchar(20) not null comment 'name',
+    `id_card` varchar(18) not null comment 'identity card',
+    `type` char(1) not null comment 'passenger type | [PassengerTypeEnum]',
+    `create_time` datetime(3) comment 'create time',
+    `update_time` datetime(3) comment 'modified time',
+    primary key (`id`),
+    index `member_id_index` (`member_id`)
+) engine=innodb default charset=utf8mb4 comment='passenger';
