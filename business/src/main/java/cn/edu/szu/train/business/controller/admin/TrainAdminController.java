876c64ebd4960fadaad6c65f1b92cdb80aa9e0ee
hojiahao <hojiahao@outlook.com>
2025-02-19 22:30:41 +0800

    开发后端接口，实现火车车次下拉功能

--- business/src/main/java/cn/edu/szu/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/cn/edu/szu/train/business/controller/admin/TrainAdminController.java
@@ -10,6 +10,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/train")
 public class TrainAdminController {
@@ -33,5 +35,11 @@ public class TrainAdminController {
 public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("query-all")
+    public CommonResp<List<TrainQueryResponse>> queryList() {
+        List<TrainQueryResponse> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 }
--- business/src/main/java/cn/edu/szu/train/business/service/TrainService.java
+++ business/src/main/java/cn/edu/szu/train/business/service/TrainService.java
@@ -23,48 +23,55 @@ import java.util.List;
 
 @Service
 public class TrainService {
-private static final Logger LOG = LoggerFactory.getLogger(TrainService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TrainService.class);
 
-@Resource
-private TrainMapper trainMapper;
+    @Resource
+    private TrainMapper trainMapper;
 
-public void save(TrainSaveReq req) {
-DateTime now = DateTime.now();
-Train train = BeanUtil.copyProperties(req, Train.class);
-if (ObjectUtil.isNull(train.getId())) {
-train.setId(SnowUtil.getSnowflakeNextId());
-train.setCreateTime(now);
-train.setUpdateTime(now);
-trainMapper.insert(train);
-} else {
-train.setUpdateTime(now);
-trainMapper.updateByPrimaryKey(train);
-}
-}
+    public void save(TrainSaveReq req) {
+        DateTime now = DateTime.now();
+        Train train = BeanUtil.copyProperties(req, Train.class);
+        if (ObjectUtil.isNull(train.getId())) {
+            train.setId(SnowUtil.getSnowflakeNextId());
+            train.setCreateTime(now);
+            train.setUpdateTime(now);
+            trainMapper.insert(train);
+        } else {
+        train.setUpdateTime(now);
+        trainMapper.updateByPrimaryKey(train);
+        }
+    }
 
-public PageResp<TrainQueryResponse> queryList(TrainQueryReq req) {
-    TrainExample trainExample = new TrainExample();
-    trainExample.setOrderByClause("id desc");
-    TrainExample.Criteria criteria = trainExample.createCriteria();
+    public PageResp<TrainQueryResponse> queryList(TrainQueryReq req) {
+        TrainExample trainExample = new TrainExample();
+        trainExample.setOrderByClause("id desc");
+        TrainExample.Criteria criteria = trainExample.createCriteria();
 
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getPageSize());
-    PageHelper.startPage(req.getPage(), req.getPageSize());
-    List<Train> trainList = trainMapper.selectByExample(trainExample);
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getPageSize());
+        PageHelper.startPage(req.getPage(), req.getPageSize());
+        List<Train> trainList = trainMapper.selectByExample(trainExample);
 
-    PageInfo<Train> pageInfo = new PageInfo<>(trainList);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
+        PageInfo<Train> pageInfo = new PageInfo<>(trainList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-    List<TrainQueryResponse> list = BeanUtil.copyToList(trainList, TrainQueryResponse.class);
+        List<TrainQueryResponse> list = BeanUtil.copyToList(trainList, TrainQueryResponse.class);
 
         PageResp<TrainQueryResponse> pageResp = new PageResp<>();
-            pageResp.setTotal(pageInfo.getTotal());
-            pageResp.setRows(list);
-            return pageResp;
-            }
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setRows(list);
+        return pageResp;
+    }
 
-            public void delete(Long id) {
-            trainMapper.deleteByPrimaryKey(id);
-            }
+    public void delete(Long id) {
+        trainMapper.deleteByPrimaryKey(id);
+    }
+
+    public List<TrainQueryResponse> queryAll() {
+        TrainExample trainExample = new TrainExample();
+        trainExample.setOrderByClause("code desc");
+        List<Train> trainList = trainMapper.selectByExample(trainExample);
+        return BeanUtil.copyToList(trainList, TrainQueryResponse.class);
+    }
 }
