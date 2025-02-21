c5d147741976c82552e755d028d2c17920c1a700
hojiahao <hojiahao@outlook.com>
2025-02-21 21:59:37 +0800

    修改每日座位页面，增加车次查询条件，修改列号的显示

--- admin/src/views/main/business/daily-train-seat.vue
+++ admin/src/views/main/business/daily-train-seat.vue
@@ -1,8 +1,8 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
-
+      <train-select-view v-model:value="params.trainCode" width="200px"></train-select-view>
+      <a-button type="primary" @click="handleQuery()">查询</a-button>
     </a-space>
   </p>
   <a-table :dataSource="dailyTrainSeats"
@@ -15,7 +15,7 @@
       </template>
       <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
-          <span v-if="item.code === record.col">
+          <span v-if="item.code === record.col && item.type === record.seatType">
             {{ item.desc }}
           </span>
         </span>
@@ -35,9 +35,11 @@
 import {defineComponent, ref, onMounted} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "daily-train-seat-view",
+  components: {TrainSelectView},
   setup() {
     const SEAT_COL_ARRAY = window.SEAT_COL_ARRAY;
     const SEAT_TYPE_ARRAY = window.SEAT_TYPE_ARRAY;
@@ -63,6 +65,9 @@ export default defineComponent({
       pageSize: 10,
     });
     let loading = ref(false);
+    let params = ref({
+      trainCode: null
+    })
     const columns = [
       {
         title: '日期',
@@ -118,7 +123,8 @@ export default defineComponent({
       axios.get("/business/admin/daily-train-seat/query-list", {
         params: {
           page: param.page,
-          pageSize: param.pageSize
+          pageSize: param.pageSize,
+          trainCode: params.value.trainCode
         }
       }).then((response) => {
         loading.value = false;
@@ -157,10 +163,11 @@ export default defineComponent({
       visible,
       dailyTrainSeats,
       pagination,
+      loading,
+      params,
       columns,
       handleTableChange,
-      handleQuery,
-      loading,
+      handleQuery
     };
   },
 });
--- business/src/main/java/cn/edu/szu/train/business/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/cn/edu/szu/train/business/req/DailyTrainSeatQueryReq.java
@@ -4,9 +4,20 @@ import cn.edu.szu.train.common.request.PageRequest;
 
 
 public class DailyTrainSeatQueryReq extends PageRequest {
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
-        return "DailyTrainSeatQueryReq{" + "}" + super.toString();
+        return "DailyTrainSeatQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
+                "} " + super.toString();
     }
 }
