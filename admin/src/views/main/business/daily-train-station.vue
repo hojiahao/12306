cec7f0837d605110ffa2531b5b31dc65b44e4d92
hojiahao <hojiahao@outlook.com>
2025-02-21 17:48:07 +0800

    完善每日车站页面，增加日期和车次查询条件

--- admin/src/views/main/business/daily-train-station.vue
+++ admin/src/views/main/business/daily-train-station.vue
@@ -1,7 +1,9 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期"/>
+      <train-select-view v-model:value="params.trainCode" width="200px"></train-select-view>
+      <a-button type="primary" @click="handleQuery()">查询</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
     </a-space>
   </p>
@@ -31,7 +33,7 @@
         <a-date-picker v-model:value="dailyTrainStation.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期"/>
       </a-form-item>
       <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrainStation.trainCode"/>
+        <train-select-view v-model:value="dailyTrainStation.trainCode"></train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="dailyTrainStation.index"/>
@@ -40,7 +42,7 @@
         <a-input v-model:value="dailyTrainStation.name"/>
       </a-form-item>
       <a-form-item label="站名拼音">
-        <a-input v-model:value="dailyTrainStation.namePinyin"/>
+        <a-input v-model:value="dailyTrainStation.namePinyin" disabled/>
       </a-form-item>
       <a-form-item label="进站时间">
         <a-time-picker v-model:value="dailyTrainStation.entryTime" valueFormat="HH:mm:ss" placeholder="请选择时间"/>
@@ -49,7 +51,7 @@
         <a-time-picker v-model:value="dailyTrainStation.exitTime" valueFormat="HH:mm:ss" placeholder="请选择时间"/>
       </a-form-item>
       <a-form-item label="停站时长">
-        <a-time-picker v-model:value="dailyTrainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间"/>
+        <a-time-picker v-model:value="dailyTrainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间" disabled/>
       </a-form-item>
       <a-form-item label="里程（公里）">
         <a-input v-model:value="dailyTrainStation.km"/>
@@ -59,12 +61,16 @@
 </template>
 
 <script>
-import {defineComponent, ref, onMounted} from 'vue';
+import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select.vue";
+import {pinyin} from "pinyin-pro";
+import dayjs from "dayjs";
 
 export default defineComponent({
   name: "daily-train-station-view",
+  components: {TrainSelectView},
   setup() {
     const visible = ref(false);
     let dailyTrainStation = ref({
@@ -89,6 +95,10 @@ export default defineComponent({
       pageSize: 10,
     });
     let loading = ref(false);
+    let params = ref({
+      date: null,
+      trainCode: null
+    })
     const columns = [
       {
         title: '日期',
@@ -140,6 +150,26 @@ export default defineComponent({
         dataIndex: 'action'
       }
     ];
+
+    watch(() => dailyTrainStation.value.name, () => {
+      if (Tool.isNotEmpty(dailyTrainStation.value.name)) {
+        dailyTrainStation.value.namePinyin = pinyin(dailyTrainStation.value.name, {toneType: 'none'}).replace(" ", "");
+      } else {
+        dailyTrainStation.value.namePinyin = "";
+      }
+    }, {immediate: true});
+
+    // 自动计算停站时长
+    watch (() => dailyTrainStation.value.entryTime, () => {
+      let diff = dayjs(dailyTrainStation.value.exitTime, "HH:mm:ss").diff(dayjs(dailyTrainStation.value.entryTime, "HH:mm:ss"), "second");
+      dailyTrainStation.value.stopTime = dayjs("00:00:00", "HH:mm:ss").second(diff).format("HH:mm:ss");
+    }, {immediate: true});
+
+    // 自动计算停站时长
+    watch (() => dailyTrainStation.value.exitTime, () => {
+      let diff = dayjs(dailyTrainStation.value.exitTime, "HH:mm:ss").diff(dayjs(dailyTrainStation.value.entryTime, "HH:mm:ss"), "second");
+      dailyTrainStation.value.stopTime = dayjs("00:00:00", "HH:mm:ss").second(diff).format("HH:mm:ss");
+    }, {immediate: true});
 
     const onAdd = () => {
       dailyTrainStation.value = {};
@@ -193,7 +223,9 @@ export default defineComponent({
       axios.get("/business/admin/daily-train-station/query-list", {
         params: {
           page: param.page,
-          pageSize: param.pageSize
+          pageSize: param.pageSize,
+          date: params.value.date,
+          trainCode: params.value.trainCode
         }
       }).then((response) => {
         loading.value = false;
@@ -230,10 +262,11 @@ export default defineComponent({
       visible,
       dailyTrainStations,
       pagination,
+      loading,
+      params,
       columns,
       handleTableChange,
       handleQuery,
-      loading,
       onAdd,
       handleOk,
       onEdit,
--- business/src/main/java/cn/edu/szu/train/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/cn/edu/szu/train/business/req/DailyTrainStationQueryReq.java
@@ -1,12 +1,38 @@
 package cn.edu.szu.train.business.req;
 
 import cn.edu.szu.train.common.request.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 
 public class DailyTrainStationQueryReq extends PageRequest {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
-        return "DailyTrainStationQueryReq{" + "}" + super.toString();
+        return "DailyTrainStationQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
+                "} " + super.toString();
     }
 }
