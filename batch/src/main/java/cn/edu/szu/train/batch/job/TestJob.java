64f39e773f5a1278a1966380d53d65649c9851ae
hojiahao <hojiahao@outlook.com>
2025-02-21 14:22:26 +0800

    禁止任务并发执行

--- batch/src/main/java/cn/edu/szu/train/batch/job/TestJob.java
+++ batch/src/main/java/cn/edu/szu/train/batch/job/TestJob.java
@@ -1,12 +1,20 @@
 package cn.edu.szu.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob Test");
+        System.out.println("TestJob Start");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob End");
     }
 }
